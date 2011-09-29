31786932a30a6f3673dc60348bd09c67dfbc0da4
Vernon Mauery <vernon@mauery.com>
2011-09-28 21:29:06 -0700

    Add readline for primitive command history
    Signed-off-by: Vernon Mauery <vernon@mauery.com>

--- hc.py
+++ hc.py
@@ -40,6 +40,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #----------------------------------
 # Python library stuff
 import sys, getopt
+import readline
 from string import strip
 from os import remove, system, urandom, environ
 import os.path
